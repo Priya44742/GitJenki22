1543ef6f611a8b1bf3c3c0b40073fd6f31632599
Priya44742 <priyapatil6393@gmail.com>
2026-04-22 13:06:38 +0530

    New line added into Demo22 class

--- GitJenkinsDemo/src/com/Demo22.java
+++ GitJenkinsDemo/src/com/Demo22.java
@@ -5,7 +5,7 @@ public class Demo22 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello This is Demo22 class");
-
+       System.out.println("New line added into main");
 	}
 
 }
